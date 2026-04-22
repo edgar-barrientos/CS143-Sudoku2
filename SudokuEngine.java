65580a5954ab39280b731168b0b34dbbf05be3ea
JesseH0630 <zxh0630@outlook.com>
2026-04-22 12:19:53 -0700

    Completed isValid(), isSolved() methods.
    Also added a lot of test cases and a SudokuEngineChecker to use the test cases and check the SudokuBoard.

--- SudokuEngine.java
+++ SudokuEngine.java
@@ -1,42 +1,25 @@
-import java.util.*;
-
 public class SudokuEngine {
-   public static void main(String[] args) {
-      SudokuBoard board = new SudokuBoard("data1.sdk");
-      System.out.print(board);
-   }
+	public static void main(String[] args) {
+		SudokuBoard board = new SudokuBoard("Sudoku2/data1.sdk");
+		System.out.print(board);
+		System.out.println("Is valid? " + board.isValid());
+		System.out.println("Is solved? " + board.isSolved());
+	}
 }
-
-
 /*
-# CHANGE LOG (to be updated)
-
-Entry | Summary   | Change
------------------------------
-  1   |           |  
-  2   |           |                                          
-  3   |           | 
-
-
-
-
-# PROGRAM OUTPUT
-
-  ----jGRASP exec: java SudokuEngine
- +-----------------------+
- | 2 . . | 1 . 5 | . . 3 |
- | . 5 4 | . . . | 7 1 . |
- | . 1 . | 2 . 3 | . 8 . |
- |-------|-------|-------|
- | 6 . 2 | 8 . 7 | 3 . 4 |
- | . . . | . . . | . . . |
- | 1 . 5 | 3 . 9 | 8 . 6 |
- |-------|-------|-------|
- | . 2 . | 7 . 1 | . 6 . |
- | . 8 1 | . . . | 2 4 . |
- | 7 . . | 4 . 2 | . . 1 |
- +-----------------------+
- 
-  ----jGRASP: Operation complete.
-
++-----------------------+
+| 2 . . | 1 . 5 | . . 3 |
+| . 5 4 | . . . | 7 1 . |
+| . 1 . | 2 . 3 | . 8 . |
+|-------|-------|-------|
+| 6 . 2 | 8 . 7 | 3 . 4 |
+| . . . | . . . | . . . |
+| 1 . 5 | 3 . 9 | 8 . 6 |
+|-------|-------|-------|
+| . 2 . | 7 . 1 | . 6 . |
+| . 8 1 | . . . | 2 4 . |
+| 7 . . | 4 . 2 | . . 1 |
++-----------------------+
+Is valid? true
+Is solved? false
 */
